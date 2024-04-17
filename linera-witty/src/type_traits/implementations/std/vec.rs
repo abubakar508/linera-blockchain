01ed754a2104f8895e9442069189e5fb7b509f8f
Abisma Dir <abubakarismail508@gmail.com>
2024-04-17 22:02:10 +0000

    add(linera connection): SSH key from gitpod

---
+++ linera-witty/src/type_traits/implementations/std/vec.rs
@@ -0,0 +1,116 @@
+// Copyright (c) Zefchain Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+//! Implementations of the custom traits for the [`Vec`] type.
+
+use std::borrow::Cow;
+
+use frunk::{hlist, hlist_pat, HList};
+
+use crate::{
+    GuestPointer, InstanceWithMemory, Layout, Memory, Runtime, RuntimeError, RuntimeMemory,
+    WitLoad, WitStore, WitType,
+};
+
+impl<T> WitType for Vec<T>
+where
+    T: WitType,
+{
+    const SIZE: u32 = 8;
+
+    type Layout = HList![i32, i32];
+    type Dependencies = HList![T];
+
+    fn wit_type_name() -> Cow<'static, str> {
+        format!("list<{}>", T::wit_type_name()).into()
+    }
+
+    fn wit_type_declaration() -> Cow<'static, str> {
+        // The native `list` type doesn't need to be declared
+        "".into()
+    }
+}
+
+impl<T> WitLoad for Vec<T>
+where
+    T: WitLoad,
+{
+    fn load<Instance>(
+        memory: &Memory<'_, Instance>,
+        location: GuestPointer,
+    ) -> Result<Self, RuntimeError>
+    where
+        Instance: InstanceWithMemory,
+        <Instance::Runtime as Runtime>::Memory: RuntimeMemory<Instance>,
+    {
+        let address = GuestPointer::load(memory, location)?;
+        let length = u32::load(memory, location.after::<GuestPointer>())?;
+
+        (0..length)
+            .map(|index| T::load(memory, address.index::<T>(index)))
+            .collect()
+    }
+
+    fn lift_from<Instance>(
+        hlist_pat![address, length]: <Self::Layout as Layout>::Flat,
+        memory: &Memory<'_, Instance>,
+    ) -> Result<Self, RuntimeError>
+    where
+        Instance: InstanceWithMemory,
+        <Instance::Runtime as Runtime>::Memory: RuntimeMemory<Instance>,
+    {
+        let address = GuestPointer(address.try_into()?);
+        let length = length as u32;
+
+        (0..length)
+            .map(|index| T::load(memory, address.index::<T>(index)))
+            .collect()
+    }
+}
+
+impl<T> WitStore for Vec<T>
+where
+    T: WitStore,
+{
+    fn store<Instance>(
+        &self,
+        memory: &mut Memory<'_, Instance>,
+        location: GuestPointer,
+    ) -> Result<(), RuntimeError>
+    where
+        Instance: InstanceWithMemory,
+        <Instance::Runtime as Runtime>::Memory: RuntimeMemory<Instance>,
+    {
+        let length = u32::try_from(self.len())?;
+        let size = length * T::SIZE;
+
+        let destination = memory.allocate(size)?;
+
+        destination.store(memory, location)?;
+        length.store(memory, location.after::<GuestPointer>())?;
+
+        self.iter()
+            .zip(0..)
+            .try_for_each(|(element, index)| element.store(memory, destination.index::<T>(index)))
+    }
+
+    fn lower<Instance>(
+        &self,
+        memory: &mut Memory<'_, Instance>,
+    ) -> Result<Self::Layout, RuntimeError>
+    where
+        Instance: InstanceWithMemory,
+        <Instance::Runtime as Runtime>::Memory: RuntimeMemory<Instance>,
+    {
+        let length = u32::try_from(self.len())?;
+        let size = length * T::SIZE;
+
+        let destination = memory.allocate(size)?;
+
+        self.iter().zip(0..).try_for_each(|(element, index)| {
+            element.store(memory, destination.index::<T>(index))
+        })?;
+
+        Ok(destination.lower(memory)? + hlist![length as i32])
+    }
+}
